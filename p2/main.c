6711980390a4977c6fd146ee9e7abf8bc8ce77c3
2025050190 <bria051@kau.kr>
2026-05-27 15:20:00 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -4,17 +4,16 @@ int main()
 {
     int x[20], y[20];
     int N;
-    int *p, *q;
 
     scanf("%d", &N);
 
-    for (p = x; p < x + N; p++)
+    for (int *p = x; p < x + N; p++)
         scanf("%d", p);
-    for (p = y; p < y + N; p++)
+    for (int *p = y; p < y + N; p++)
         scanf("%d", p);
 
 
-    for (p = x, q = y+N-1; p < x + N; p++, q--) {
+    for (int *p = x, *q = y+N-1; p < x + N; p++, q--) {
         printf(" %d", *p + *q);
     }
 
