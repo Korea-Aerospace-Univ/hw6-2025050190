76962e6a161c6b684bcba954e5c2ab46eec223fd
2025050190 <bria051@kau.kr>
2026-05-27 15:18:39 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -5,15 +5,14 @@ int main()
     char x[10] = {};
     int count = 0, flag = 0;
     char ch;
-    char *p, *q;
 
-    for (p = x; p < x + 10; p++)
+    for (char *p = x; p < x + 10; p++)
         scanf("%c", p);
 
-    for (p = x; p < x + 10; p++) {
+    for (char *p = x; p < x + 10; p++) {
 
         flag = 0;
-        for (q = x; q < x + 10; q++) {
+        for (char *q = x; q < x + 10; q++) {
             if (*p == *q) {
                 flag ++;
 
